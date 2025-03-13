a83ff13a5c7f6faedf654ca5f93cb45a873afbaf
Bohdan Astakhov <109680635+pippsza@users.noreply.github.com>
2025-03-14 01:16:50 +0200

    dsadasd

--- src/script.js
+++ src/script.js
@@ -1,5 +1,42 @@
+import axios from 'axios';
 // https://ddnet.org/players/?json2=Cor
+
 // import axios from 'axios';
+
+// export const fetchPhotosByQuery = async (searchedQuery, currentPage) => {
+//   const axiosOptions = {
+//     params: {
+//       q: searchedQuery,
+//       key: '48288384-c73711b953ffb418f1a2cd50e',
+//       image_type: 'photo',
+//       orientation: 'horizontal',
+//       page: currentPage,
+//       per_page: 15,
+//       safesearch: 'true',
+//     },
+//   };
+
+//   const response = await axios.get(`https://pixabay.com/api/`, axiosOptions);
+
+//   return response.data;
+// };
+const playerName = 'Cor';
+const getTeeInfo = async () => {
+  const axiosOptions = {
+    params: {
+      json2: playerName,
+    },
+  };
+
+  const response = await axios.get(
+    `https://ddnet.org/players/
+`,
+    axiosOptions
+  );
+  console.log(response);
+  return response;
+};
+
 const button = document.querySelector('.js-button');
 const input = document.querySelector('.js-input');
 const list = document.querySelector('.js-list');
